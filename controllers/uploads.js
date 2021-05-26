f8e6bbe31d41118a09c8b6385f468dd8bfebe1cc
andres <andres.santibanezp@gmail.com>
2021-05-25 21:07:42 -0400

    file upload added cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -2,6 +2,9 @@ const { response } = require("express");
 const path = require("path");
 const fs = require('fs');
 
+const cloudinary = require('cloudinary').v2
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const { subirArchivo } = require("../helpers");
 const { Usuario, Producto } = require('../models');
 
@@ -89,6 +92,62 @@ const actualizarImagen = async (req, res = response) => {
 }
 
 
+const actualizarImagenCloudinary = async (req, res = response) => {
+
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+
+    switch (coleccion) {
+
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id: ${id}`
+                })
+            }
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                })
+            }
+            break;
+
+        default:
+            return res.status(500).json({ msg: `Se me olvidó validar esto` });
+    }
+
+
+    // limpiar imagenes previas de cloudinary
+    if(modelo.img){
+
+        const nombreArr = modelo.img.split('/');
+        const nombre = nombreArr[nombreArr.length - 1]
+        const [ public_id ] =  nombre.split('.');
+
+        cloudinary.uploader.destroy(public_id);
+
+    }
+
+    const { tempFilePath } = req.files.archivo;
+    const { secure_url } = await cloudinary.uploader.upload( tempFilePath );
+
+
+    modelo.img = secure_url;
+    modelo.save();
+
+
+    res.json(modelo);
+
+}
+
 
 const mostrarImagen = async(req, res = response) => {
 
@@ -145,5 +204,6 @@ const mostrarImagen = async(req, res = response) => {
 module.exports = {
     cargarArchivo,
     actualizarImagen,
+    actualizarImagenCloudinary,
     mostrarImagen
 }
